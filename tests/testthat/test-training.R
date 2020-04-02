8ff5383634ccd4fab79938d756edf27fd5511b30
Johan Larsson <mail@larssonjohan.com>
2020-04-02 11:38:37 +0200

    speed up test

--- tests/testthat/test-training.R
+++ tests/testthat/test-training.R
@@ -2,7 +2,7 @@ test_that("model training works with trainSLOPE", {
   set.seed(48)
 
   for (family in c("gaussian", "binomial")) {
-    xy <- SLOPE:::randomProblem(1e3, 4, response = family)
+    xy <- SLOPE:::randomProblem(1e2, 2, response = family)
 
     fit <- trainSLOPE(xy$x,
                       xy$y,
@@ -15,19 +15,19 @@ test_that("model training works with trainSLOPE", {
   }
 })
 
-test_that("erroneous input throws errors in plot.trainSLOPE", {
-  xy <- SLOPE:::randomProblem(1e3, 2)
+test_that("plot.trainSLOPE works as expected", {
+  xy <- SLOPE:::randomProblem(1e2, 2)
   x <- xy$x
   y <- xy$y
 
-  fit <- trainSLOPE(x, y)
+  fit <- trainSLOPE(x, y, solver = "admm")
 
   expect_error(plot(fit, measure = "auc"))
   p <- plot(fit)
   expect_s3_class(p, "trellis")
-  expect_silent(dont_plot(p))
+  expect_silent(plot(p))
 
-  fit <- trainSLOPE(x, y, q = c(0.1, 0.2), number = 2)
+  fit <- trainSLOPE(x, y, q = c(0.1, 0.2), number = 2, solver = "admm")
 
   p <- plot(fit)
   expect_s3_class(p, "trellis")
