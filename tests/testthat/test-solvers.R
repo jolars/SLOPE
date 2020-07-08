ae5a4e19269cdc3db184a82a35c5c9191df0d55d
Johan Larsson <mail@larssonjohan.com>
2020-07-08 08:47:38 +0200

    improve precision of estimates in solvers test

--- tests/testthat/test-solvers.R
+++ tests/testthat/test-solvers.R
@@ -1,8 +1,10 @@
 test_that("different solvers return equivalent output", {
   set.seed(853)
 
-  admm <- SLOPE(bodyfat$x, bodyfat$y, solver = "admm")
-  fista <- SLOPE(bodyfat$x, bodyfat$y, solver = "fista")
+  admm <- SLOPE(bodyfat$x, bodyfat$y, solver = "admm",
+                tol_rel = 1e-6, tol_abs = 1e-7)
+  fista <- SLOPE(bodyfat$x, bodyfat$y, solver = "fista",
+                 tol_rel_gap = 1e-9)
 
   expect_equivalent(coef(admm), coef(fista), tol = 1e-3)
 })
