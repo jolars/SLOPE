1636e267cff59486fe0e75fcd7a0099bd7c3b2f3
Johan Larsson <johanlarsson@outlook.com>
2021-11-17 15:57:29 +0100

    Format and remove unnecessary arma namespace

--- src/r-exports.cpp
+++ src/r-exports.cpp
@@ -1,13 +1,11 @@
-#include <RcppArmadillo.h>
 #include "prox.h"
+#include <RcppArmadillo.h>
 
 using namespace Rcpp;
-using namespace arma;
 
 // [[Rcpp::export]]
-arma::mat sorted_l1_prox(const arma::mat& x, const arma::vec& lambda)
+arma::mat
+sorted_l1_prox(const arma::mat& x, const arma::vec& lambda)
 {
   return prox(x, lambda);
 }
-
-
