06b242dfcd9ad684f6e00c6a6f57d6b0f6489a0a
jakubkala <kalajakub96@gmail.com>
2022-01-27 17:04:48 +0100

    ABSLOPE text example

---
+++ tests/testthat/test-abslope.R
@@ -0,0 +1,11 @@
+
+
+test_that("ABSLOPE", {
+
+  set.seed(17)
+  xy <- SLOPE:::randomProblem(1e2, 2, response = "gaussian")
+  X <- as.matrix(xy$x)
+  Y <- xy$y
+  fit <- ABSLOPE(X, Y)
+  assert(fit$beta == c(V1 = NaN, V2 = NaN))
+})
