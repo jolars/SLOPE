9731aa74a7cc695e88d2ce266a48fd856565ccc7
Johan Larsson <johanlarsson@outlook.com>
2022-06-07 13:23:26 +0200

    chore: use lower-case for arguments everywhere

--- R/ABSLOPE.R
+++ R/ABSLOPE.R
@@ -15,13 +15,13 @@ rescale <- function(y, x) {
 #' rescale_all
 #'
 #' @param results A number
-#' @param Xmis A number
+#' @param x_miss A number
 #'
 #' @keywords internal
 
-rescale_all <- function(results, Xmis) {
+rescale_all <- function(results, x_miss) {
   k <- NCOL(results[["X"]])
-  scales <- sapply(1:k, function(l) rescale(results[["X"]][, l], Xmis[, l]))
+  scales <- sapply(1:k, function(l) rescale(results[["X"]][, l], x_miss[, l]))
   results[["X"]] <- t(t(results[["X"]]) * scales[2, ] + scales[1, ])
   results[["Sigma"]] <- results[["Sigma"]] * (scales[2, ] %*% t(scales[2, ]))
   results[["mu"]] <- results[["mu"]] * scales[2] + scales[1, ]
@@ -40,31 +40,30 @@ rescale_all <- function(results, Xmis) {
 #'   and handle missing values by Stochastic Approximation of Expected
 #'   Maximization (SAEM)
 #'
-#' @param start the initial vector of regression coefficients for the first
+#' @param x_miss model matrix
+#' @param x_init model matrix with initially imputed NA values
+#' @param beta_start the initial vector of regression coefficients for the first
 #' iteration. Default to the LASSO estimator obtained after
-#'
-#' @param Xmis model matrix
-#' @param Xinit model matrix with initially imputed NA values
-#' @param Y numeric. Response variable.
+#' @param y numeric. Response variable.
 #' @param a_prior,b_prior non-negative parameters of the prior Beta distribution
 #'   on theta.
-#' @param Covmat numeric covariance matrix. Default to identity matrix.
+#' @param covmat numeric covariance matrix. Default to identity matrix.
 #' @param sigma the variance of the noise. Default to 1.
-#' @param FDR False Discovery Rate. Default to 0.05.
+#' @param fdr False Discovery Rate. Default to 0.05.
+#' @param bh logical. Indicates whether the Benjamini-Hochberg correction for
+#'   multiple testing should be used.
 #' @param tol optimization tolerance.
 #' @param max_iter the maximal number of iterations of the optimization
 #'   algorithm. Default to 100.
 #' @param verbose verbosity. Default to FALSE
-#' @param BH logical. Indicates whether the Benjamini-Hochberg correction for
-#'   multiple testing should be used.
 #'
 #' @details \code{ABSLOPE} is the combination of SLOPE and Spike-and-Slab
 #'   LASSO (SSL). This approach relies on iterations of the weighted SLOPE
 #'   algorithm and finds the solution by minimizing
 #'   \deqn{
-#'    G(Y,X,\beta) + \sum_{j = 1}^{p} w_j \lambda_{r(\beta,j)}|\beta_j|,
+#'    G(y,X,\beta) + \sum_{j = 1}^{p} w_j \lambda_{r(\beta,j)}|\beta_j|,
 #'   }{
-#'    G(Y,X,\beta) + \sum w_j |\beta_j|\lambda_r(\beta,j),
+#'    G(y,X,\beta) + \sum w_j |\beta_j|\lambda_r(\beta,j),
 #'   }
 #'   where \eqn{r(\beta,j) = {1,2,...,p}} is the rank of \eqn{\beta_j}  among
 #'   elements in \eqn{\beta} in a descending order. The weight \eqn{w_j} depends
@@ -90,49 +89,49 @@ rescale_all <- function(results, Xmis) {
 #' @examples
 #' set.seed(17)
 #' xy <- SLOPE:::randomProblem(1e2, 200, response = "gaussian")
-#' X <- as.matrix(xy$x)
-#' Y <- xy$y
-#' fit <- ABSLOPE(X, Y)
+#' x <- as.matrix(xy$x)
+#' y <- xy$y
+#' fit <- ABSLOPE(x, y)
 #' @export ABSLOPE
 #'
 
-ABSLOPE <- function(Xmis,
-                    Y,
-                    start = NULL,
-                    Xinit = NULL,
-                    a_prior = 0.01 * NROW(Xmis),
-                    b_prior = 0.01 * NROW(Xmis),
-                    Covmat = NULL,
+ABSLOPE <- function(x_miss,
+                    y,
+                    x_init = NULL,
+                    beta_start = NULL,
+                    a_prior = 0.01 * NROW(x_miss),
+                    b_prior = 0.01 * NROW(x_miss),
+                    covmat = NULL,
                     sigma = NULL,
-                    FDR = 0.05,
+                    fdr = 0.05,
+                    bh = TRUE,
                     tol = 1e-04,
                     max_iter = 100L,
-                    verbose = FALSE,
-                    BH = TRUE) {
+                    verbose = FALSE) {
   checkmate::assert_number(a_prior)
   checkmate::assert_number(b_prior)
-  checkmate::assert_number(FDR)
+  checkmate::assert_number(fdr)
   checkmate::assert_number(tol)
   checkmate::assert_number(max_iter)
   checkmate::assert_logical(verbose)
-  checkmate::assert_logical(BH)
+  checkmate::assert_logical(bh)
 
-  if (!(is.matrix(Xmis) | is.data.frame(Xmis))) {
+  if (!(is.matrix(x_miss) | is.data.frame(x_miss))) {
     stop(
-      "Xmis needs to be matrix or data.frame. You provided ",
-      paste0(class(Xmis), collapse = ", ")
+      "x_miss needs to be matrix or data.frame. You provided ",
+      paste0(class(x_miss), collapse = ", ")
     )
   }
 
-  Xmis <- as.matrix(Xmis)
+  x_miss <- as.matrix(x_miss)
 
   ocall <- match.call()
 
   # if Covmat is null -> known_cov = FALSE
-  known_cov <- !is.null(Covmat)
+  known_cov <- !is.null(covmat)
   # dummy value for a case with unknown covariance matrix
-  if (is.null(Covmat)) {
-    Covmat <- diag(NCOL(Xmis))
+  if (is.null(covmat)) {
+    covmat <- diag(NCOL(x_miss))
   }
   # if sigma is null -> known_sigma = FALSE
   known_sigma <- !is.null(sigma)
@@ -141,38 +140,43 @@ ABSLOPE <- function(Xmis,
     sigma <- 1
   }
 
-  # if Xinit is null -> mice imputation
-  if (is.null(Xinit)) {
-    imp <- mice::mice(Xmis, m = 1, printFlag = FALSE)
-    Xinit <- as.matrix(mice::complete(imp))
+  # if x_init is null -> mice imputation
+  if (is.null(x_init)) {
+    imp <- mice::mice(x_miss, m = 1, printFlag = FALSE)
+    x_init <- as.matrix(mice::complete(imp))
   }
 
-  # if start is null -> LASSO gives starting coefficients
-  if (is.null(start)) {
-    lasso <- glmnet::cv.glmnet(Xinit, Y, standardize = FALSE, intercept = FALSE)
-    start <- stats::coefficients(lasso, s = "lambda.min")
-    start <- start[2:(NCOL(Xinit) + 1), 1]
+  # if beta_start is null -> LASSO gives beta_starting coefficients
+  if (is.null(beta_start)) {
+    lasso <- glmnet::cv.glmnet(
+      x_init,
+      y,
+      standardize = FALSE,
+      intercept = FALSE
+    )
+    beta_start <- stats::coefficients(lasso, s = "lambda.min")
+    beta_start <- beta_start[2:(NCOL(x_init) + 1), 1]
   }
 
   out <- SLOBE_ADMM_approx_missing(
-    start,
-    Xmis,
-    Xinit,
-    Y,
+    beta_start,
+    x_miss,
+    x_init,
+    y,
     a_prior,
     b_prior,
-    Covmat,
+    covmat,
     sigma,
-    FDR,
+    fdr,
     tol,
     known_sigma,
     max_iter,
     verbose,
-    BH,
+    bh,
     known_cov
   )
 
-  out <- rescale_all(out, Xmis)
+  out <- rescale_all(out, x_miss)
   out[["call"]] <- ocall
 
   structure(
--- R/print.R
+++ R/print.R
@@ -55,9 +55,9 @@ print.TrainedSLOPE <- function(x, ...) {
 #' @examples
 #' set.seed(17)
 #' xy <- SLOPE:::randomProblem(1e2, 2, response = "gaussian")
-#' X <- as.matrix(xy$x)
-#' Y <- xy$y
-#' fit <- ABSLOPE(X, Y)
+#' x <- as.matrix(xy$x)
+#' y <- xy$y
+#' fit <- ABSLOPE(x, y)
 #' print(fit)
 #'
 #' @method print ABSLOPE
