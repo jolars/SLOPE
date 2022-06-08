6c89ac388b5676e539b2a27d528684bcf6ad7419
Johan Larsson <johanlarsson@outlook.com>
2022-06-08 09:16:56 +0200

    chore(style): autoformat and reformat

--- R/plot.R
+++ R/plot.R
@@ -23,15 +23,19 @@
 #' plot(fit)
 plot.SLOPE <- function(x,
                        intercept = FALSE,
-                       x_variable = c("alpha",
-                                      "deviance_ratio",
-                                      "step"),
+                       x_variable = c(
+                         "alpha",
+                         "deviance_ratio",
+                         "step"
+                       ),
                        ...) {
   object <- x
 
-  if(inherits(object, "ABSLOPE")) {
-    stop("There's no plot functionality for ABSLOPE objects -
-         x must be fitted by SLOPE function.")
+  if (inherits(object, "ABSLOPE")) {
+    stop(
+      "There's no plot functionality for ABSLOPE objects. ",
+      "x must be fitted by SLOPE()."
+    )
   }
 
   x_variable <- match.arg(x_variable)
@@ -50,36 +54,41 @@ plot.SLOPE <- function(x,
   p <- NROW(coefs) # number of features
   m <- NCOL(coefs) # number of responses
 
-  x <- switch(x_variable,
-              alpha = object[["alpha"]],
-              deviance_ratio = object[["deviance_ratio"]],
-              step = seq_along(object[["alpha"]]))
+  x <- switch(
+    x_variable,
+    alpha = object[["alpha"]],
+    deviance_ratio = object[["deviance_ratio"]],
+    step = seq_along(object[["alpha"]])
+  )
 
-  xlab <- switch(x_variable,
-                 alpha = expression(alpha),
-                 deviance_ratio = "Fraction Deviance Explained",
-                 step = "Step")
-
+  xlab <- switch(
+    x_variable,
+    alpha = expression(alpha),
+    deviance_ratio = "Fraction Deviance Explained",
+    step = "Step"
+  )
 
   d <- as.data.frame(as.table(coefs))
   d[["x"]] <- rep(x, each = p * m)
 
-  plt <- ggplot2::ggplot(d, ggplot2::aes(x = !!quote(x),
-                                         y = !!quote(Freq),
-                                         col = !!quote(Var1))) +
+  plt <- ggplot2::ggplot(
+    d,
+    ggplot2::aes(
+      x = !!quote(x),
+      y = !!quote(Freq),
+      col = !!quote(Var1)
+    )
+  ) +
     ggplot2::geom_line() +
     ggplot2::ylab(expression(hat(beta))) +
     ggplot2::xlab(xlab) +
-    ggplot2::labs(color = 'Variable name')
+    ggplot2::labs(color = "Variable name")
 
-  if(m > 1) {
-
-    plt <- plt + ggplot2::facet_wrap(~!!quote(Var2))
-
+  if (m > 1) {
+    plt <- plt + ggplot2::facet_wrap(~ !!quote(Var2))
   }
 
   plt
-
 }
 
 #' Plot results from cross-validation
@@ -110,29 +119,36 @@ plot.SLOPE <- function(x,
 #' # Cross-validation for a SLOPE binomial model
 #' set.seed(123)
 #' tune <- trainSLOPE(subset(mtcars, select = c("mpg", "drat", "wt")),
-#'                    mtcars$hp,
-#'                    q = c(0.1, 0.2),
-#'                    number = 10)
+#'   mtcars$hp,
+#'   q = c(0.1, 0.2),
+#'   number = 10
+#' )
 #' plot(tune, ci_col = "salmon")
 plot.TrainedSLOPE <- function(x,
-                              measure = c("auto",
-                                          "mse",
-                                          "mae",
-                                          "deviance",
-                                          "auc",
-                                          "misclass"),
+                              measure = c(
+                                "auto",
+                                "mse",
+                                "mae",
+                                "deviance",
+                                "auc",
+                                "misclass"
+                              ),
                               plot_min = TRUE,
                               ci_alpha = 0.2,
                               ci_border = FALSE,
                               ci_col = "salmon",
                               ...) {
-
-  if(!(ci_col %in% grDevices::colors()))
+  if (!(ci_col %in% grDevices::colors())) {
     stop("ci_col", ci_col, "is not a valid color representation.")
+  }
 
-  if(!(ci_border %in% grDevices::colors() | is.logical(ci_border)))
-    stop("ci_border is", ci_border, "when it should be logical or a valid
-           color representation.")
+  if (!(ci_border %in% grDevices::colors() | is.logical(ci_border))) {
+    stop(
+      "ci_border is",
+      ci_border,
+      "when it should be logical or a validcolor representation."
+    )
+  }
 
   object <- x
   family <- object[["model"]][["family"]]
@@ -140,33 +156,37 @@ plot.TrainedSLOPE <- function(x,
   measure <- match.arg(measure)
 
   if (measure == "auto") {
-    measure <- switch(
-      family,
+    measure <- switch(family,
       gaussian = "mse",
       binomial = "deviance",
       multinomial = "deviance",
       poisson = "mse"
     )
 
-    if (!any(measure %in% object[["measure"]][["measure"]]))
+    if (!any(measure %in% object[["measure"]][["measure"]])) {
       measure <- object[["measure"]][["measure"]][1]
+    }
   }
 
-  if (!any(measure %in% object[["measure"]][["measure"]]))
-    stop("measure ", measure, " was not used or not available when",
-         "fitting the model")
+  if (!any(measure %in% object[["measure"]][["measure"]])) {
+    stop(
+      "measure ",
+      measure,
+      " was not used or not available when",
+      "fitting the model"
+    )
+  }
 
-  if (length(measure) > 1)
+  if (length(measure) > 1) {
     stop("you are only allowed to plot one measure at a time")
-
+  }
 
   measure_label <-
     object[["measure"]][["label"]][object[["measure"]][["measure"]] == measure]
   summary <-
-    object[["summary"]][object[["summary"]][["measure"]] == measure,]
-  optimum <-
-    object[["optima"]][object[["optima"]][["measure"]] == measure, ,
-                       drop = FALSE]
+    object[["summary"]][object[["summary"]][["measure"]] == measure, ]
+  opt <- object[["optima"]]
+  optimum <- opt[opt[["measure"]] == measure, , drop = FALSE]
 
   optimum[["label_q"]] <- paste0("q = ", as.factor(optimum[["q"]]))
   model <- object[["model"]]
@@ -178,33 +198,40 @@ plot.TrainedSLOPE <- function(x,
 
   xlab <- expression(log[e](alpha))
 
-  p <- ggplot2::ggplot(summary, ggplot2::aes(x = log(!!quote(alpha)),
-                                             y = mean)) +
+  p <- ggplot2::ggplot(
+    summary,
+    ggplot2::aes(x = log(!!quote(alpha)), y = mean)
+  ) +
     ggplot2::geom_line() +
     ggplot2::xlab(xlab) +
     ggplot2::ylab(measure_label)
 
   if (length(q) > 1) {
-
     p <- p + ggplot2::facet_wrap(~label_q)
   }
 
-  if(plot_min) {
-
-    p <- p + ggplot2::geom_vline(data = optimum,
-                                 ggplot2::aes(xintercept = log(!!quote(alpha))),
-                                 linetype = "dotted")
+  if (plot_min) {
+    p <- p + ggplot2::geom_vline(
+      data = optimum,
+      ggplot2::aes(xintercept = log(!!quote(alpha))),
+      linetype = "dotted"
+    )
   }
 
-  border_col <- c(ci_border, NA, ci_col)[is.character(ci_border) +
-                                           2 * isFALSE(ci_border) +
-                                           3 * isTRUE(ci_border)]
+  border_col <-
+    c(
+      ci_border,
+      NA,
+      ci_col
+    )[is.character(ci_border) + 2 * isFALSE(ci_border) + 3 * isTRUE(ci_border)]
 
-  p <- p + ggplot2::geom_ribbon(ggplot2::aes(ymin = !!quote(lo),
-                                             ymax = !!quote(hi)),
-                                fill = ci_col,
-                                color = border_col,
-                                alpha = ci_alpha)
+  p <- p +
+    ggplot2::geom_ribbon(
+      ggplot2::aes(ymin = !!quote(lo), ymax = !!quote(hi)),
+      fill = ci_col,
+      color = border_col,
+      alpha = ci_alpha
+    )
 
   p
 }
