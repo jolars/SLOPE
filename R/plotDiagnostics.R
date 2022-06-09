3a08146b6cc0d3a301922e04aeb5218ee2f92934
Johan Larsson <johanlarsson@outlook.com>
2022-06-09 12:25:40 +0200

    chore: reformat to tidyverse style

--- R/plotDiagnostics.R
+++ R/plotDiagnostics.R
@@ -21,47 +21,61 @@
 #' plotDiagnostics(x)
 plotDiagnostics <- function(object,
                             ind = max(object$diagnostics$penalty),
-                            xvar = c("time",
-                                     "iteration")) {
-
-  stopifnot(inherits(object, "SLOPE"),
-            is.numeric(ind),
-            length(ind) == 1)
+                            xvar = c("time", "iteration")) {
+  stopifnot(
+    inherits(object, "SLOPE"),
+    is.numeric(ind),
+    length(ind) == 1
+  )
 
   xvar <- match.arg(xvar)
 
-  if (is.null(object$diagnostics))
-    stop("no diagnostics found in fit;",
-         "did you call SLOPE() with diagnostics = TRUE?")
+  if (is.null(object$diagnostics)) {
+    stop(
+      "no diagnostics found in fit; ",
+      "did you call SLOPE() with diagnostics = TRUE?"
+    )
+  }
 
   d <- object$diagnostics
 
   d <- subset(d, subset = d$penalty == ind)
 
-  d <- stats::reshape(d,
-                      direction = "long",
-                      varying = c("primal", "dual"),
-                      v.names = "Value",
-                      idvar = c("iteration", "time", "penalty"),
-                      timevar = "Variable",
-                      times = c("primal", "dual"))
+  d <- stats::reshape(
+    d,
+    direction = "long",
+    varying = c("primal", "dual"),
+    v.names = "Value",
+    idvar = c("iteration", "time", "penalty"),
+    timevar = "Variable",
+    times = c("primal", "dual")
+  )
 
   if (xvar == "time") {
-    plt <- ggplot2::ggplot(d, ggplot2::aes(x = !!quote(time),
-                                           y = !!quote(Value),
-                                           col = !!quote(Variable))) +
+    plt <- ggplot2::ggplot(
+      d, ggplot2::aes(
+        x = !!quote(time),
+        y = !!quote(Value),
+        col = !!quote(Variable)
+      )
+    ) +
       ggplot2::xlab("Time (seconds)")
-
   } else if (xvar == "iteration") {
-    plt <- ggplot2::ggplot(d, ggplot2::aes(x = !!quote(iteration),
-                                           y = !!quote(Value),
-                                           col = !!quote(Variable))) +
+    plt <- ggplot2::ggplot(
+      d, ggplot2::aes(
+        x = !!quote(iteration),
+        y = !!quote(Value),
+        col = !!quote(Variable)
+      )
+    ) +
       ggplot2::xlab("Iteration")
   }
 
   if (nrow(d) <= 1) {
-    plt <- plt + ggplot2::theme(panel.grid.major = ggplot2::element_blank(),
-                                panel.grid.minor = ggplot2::element_blank())
+    plt <- plt + ggplot2::theme(
+      panel.grid.major = ggplot2::element_blank(),
+      panel.grid.minor = ggplot2::element_blank()
+    )
   }
 
   plt <- plt +
