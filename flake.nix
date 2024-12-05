ecb9290b02785a1193248160a9fce24f03488065
Johan Larsson <jolars@posteo.com>
2024-12-05 14:51:41 +0100

    chore: add nix flake files

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.bashInteractive
+            (pkgs.rWrapper.override {
+              packages = with pkgs.rPackages; [
+                Matrix
+                Rcpp
+                RcppArmadillo
+                bench
+                caret
+                covr
+                devtools
+                doParallel
+                dplyr
+                foreach
+                ggplot2
+                glmnet
+                knitr
+                knockoff
+                languageserver
+                patchwork
+                rmarkdown
+                scales
+                spelling
+                stringr
+                testthat
+                tidyr
+                tidyverse
+                usethis
+                vdiffr
+              ];
+            })
+          ];
+        };
+      }
+    );
+}
