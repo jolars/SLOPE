0c54746b8a68623f96dc52de061e6e21947e51c4
Johan Larsson <jolars@posteo.com>
2025-03-05 21:38:14 +0100

    chore: autoformat

--- R/utils.R
+++ R/utils.R
@@ -12,23 +12,23 @@ camelCase <- function(x) {
   })
 }
 
-randomProblem <- function(n = 1000,
-                          p = 100,
-                          q = 0.2,
-                          n_groups = NULL,
-                          n_targets =
-                            if (match.arg(response) == "multinomial") 3 else 1,
-                          density = 1,
-                          amplitude =
-                            if (match.arg(response) == "poisson") 1 else 3,
-                          alpha = 1,
-                          response = c(
-                            "gaussian",
-                            "binomial",
-                            "poisson",
-                            "multinomial"
-                          ),
-                          rho = 0) {
+randomProblem <- function(
+  n = 1000,
+  p = 100,
+  q = 0.2,
+  n_groups = NULL,
+  n_targets = if (match.arg(response) == "multinomial") 3 else 1,
+  density = 1,
+  amplitude = if (match.arg(response) == "poisson") 1 else 3,
+  alpha = 1,
+  response = c(
+    "gaussian",
+    "binomial",
+    "poisson",
+    "multinomial"
+  ),
+  rho = 0
+) {
   m <- n_targets
 
   if (density == 1) {
@@ -42,7 +42,8 @@ randomProblem <- function(n = 1000,
   }
 
   if (!is.null(n_groups)) {
-    groups <- rep(seq_len(n_groups),
+    groups <- rep(
+      seq_len(n_groups),
       each = ceiling(m * p / n_groups),
       length.out = p * m
     )
